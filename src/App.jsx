ccde56922efa440468005f69d6a3a62c2f09139a
ManiacAyu <Ayush Dhoke>
2025-02-01 00:22:47 +0530

    twentyfifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-import { useState } from "react";
-import Page1 from "./pages/Page1";
-import Page2 from "./pages/Page2";
-import Page3 from "./pages/Page3";
-import Page4 from "./pages/Page4";
-import Page5 from "./pages/Page5";
-import Page6 from "./pages/Page6";
-import Page7 from "./pages/Page7";
-import Page8 from "./pages/Page8";
-import Page9 from "./pages/Page9";
-import Page10 from "./pages/Page10";
-import Page11 from "./pages/Page11";
+import "./App.css";
+import { About } from "./components/about/About";
+import VideoIntroductions from "./components/about/VideoIntroductions";
+import WhyChooseUs from "./components/about/WhyChooseUs";
+import Blogs from "./components/blogs/Blogs";
+import { Customer } from "./components/customer/Customer";
+import FAQs from "./components/FAQ/FAQs";
+import { Footer } from "./components/footer/Footer";
+import { Hero } from "./components/hero/Hero";
+import { Navbar } from "./components/navbar/Navbar";
+import { Header } from "./components/services/Header";
+import { Services } from "./components/services/Services";
+import { Subscribe } from "./components/subscribe/Subscribe";
 
 function App() {
   return (
-    <>
-      <Page1 />
-      <Page2 />
-      <Page3 />
-      <Page4 />
-      <Page5 />
-      <Page6 />
-      <Page7 />
-      <Page8 />
-      <Page9 />
-      <Page10 />
-      <Page11 />
-    </>
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Header/>
+      <Services />
+      <About />
+      <WhyChooseUs/>
+      <VideoIntroductions/>
+      <Blogs/>
+      <FAQs/>
+      <Subscribe />
+      <Customer />
+      <Footer />
+    </div>
   );
 }
 
